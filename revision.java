61c2bc8a8f352918cfbd7abc75785c2c323169ec
Fida-Ukwishaka <fidaukwishaka@gmail.com>
2025-10-17 00:09:11 +0000

    TBC

---
+++ revision.java
@@ -0,0 +1,12 @@
+public class revision {
+    public static void main(String[] args) {
+        int [] arr = new int[5];
+        for(int i=0;i<5;i++){
+            arr[i]=i;
+        }
+        for(int i:arr){
+            System.out.print(i);
+        }
+    }
+    double [] mylist = {1.9,2.9,3.4,3.5};
+}
